2de48cc86a36f0e5d2537b254bb7f37049943f60
ods <devnull@localhost>
2009-04-27 15:52:26 +0400

    Fetch fresh data

--- update_ru.py
+++ update_ru.py
@@ -4,6 +4,9 @@
 from update import IPTree
 from iso3166_2_ru import cc2name
 import codecs, logging, os, sys
+from urllib2 import urlopen
+from cStringIO import StringIO
+from zipfile import ZipFile
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +14,20 @@ logger = logging.getLogger(__name__)
 class RussiaRegionByIP(IPTree):
 
     value_length = 3
+    url = 'http://ipgeobase.ru/files/db/Main/db_files.zip'
     
+    def fetch(self):
+        logger.info('Fetching from %r', self.url)
+        zip_content = urlopen(self.url).read()
+        zip = ZipFile(StringIO(zip_content))
+        data = zip.read('cidr_ru_block.txt')
+        zip.close()
+        self.parse(StringIO(data))
+
     def parse(self, file):
-        fp = codecs.getreader('windows-1251')(open(file, 'rb'))
+        if isinstance(file, basestring):
+            file = open(file, 'rb')
+        fp = codecs.getreader('windows-1251')(file)
         region2code = dict((name, code) for code, name in cc2name.items())
         prev_start = 0
         prev_end = 0
@@ -38,7 +52,7 @@ class RussiaRegionByIP(IPTree):
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO)
     tree = RussiaRegionByIP()
-    tree.parse('cidr_ru_block.txt')
+    tree.fetch()
     tree.optimize()
     db_file = os.path.join(os.path.dirname(sys.argv[0]), 'ip2ru.db')
     db = open(db_file+'.new', 'wb')
