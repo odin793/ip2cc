5230ebaea13300db1da0565842e2008f8171c9ef
Viktor Kotseruba <barbuzaster@gmail.com>
2009-07-03 14:10:52 +0400

    wow, ruby has shortcut for "slice"

--- ip2cc.rb
+++ ip2cc.rb
@@ -7,19 +7,22 @@ class IP2CC
   end
   
   def [](ip, opts={})
-    raise 'invalid ip' unless ip =~ VALID_IP
+    if not ip =~ VALID_IP then
+      raise 'invalid ip' if opts[:raise]
+      return nil
+    end
     open(@filename) do |f|
       offset = 0
       ip.split('.').each do |part|
        start = offset + part.to_i * 4
        f.seek start
        value = f.read 4
-       if value.slice(0, 2) == "\xFF\xFF" then
-         if value.slice(2, 2) == "\x00\x00" then
+       if value[0, 2] == "\xFF\xFF" then
+         if value[2, 2] == "\x00\x00" then
            raise 'not found' if opts[:raise] 
            return nil
          end
-         return value.slice(2, 2)
+         return value[2, 2]
        end
        offset = value.unpack('N')[0]
       end
@@ -32,7 +35,7 @@ end
 if __FILE__ == $0 then
   db = IP2CC.new 'ip2cc.db'
   if ARGV.length == 1 then
-    puts db[ARGV[0]]
+    puts db[ARGV[0], :raise => true]
   else
     ARGV.each{ |ip| puts "#{ip} #{db[ip]}" }
   end
