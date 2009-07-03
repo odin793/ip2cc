330224cccdc5ad89548a6216c802d4d93e687cda
Viktor Kotseruba <barbuzaster@gmail.com>
2009-07-03 23:37:18 +0400

    ruby client improvements by maxlapshin

--- ip2cc.rb
+++ ip2cc.rb
@@ -1,27 +1,22 @@
-VALID_IP = Regexp.new('^'+(['(?:(?:2[0-4]|1\d|[1-9])?\d|25[0-5])']*4).join('\.')+'$')
-
-class IP2CC
+class Ip2cc
+  
+  VALID_IP = Regexp.new('^'+(['(?:(?:2[0-4]|1\d|[1-9])?\d|25[0-5])']*4).join('\.')+'$')
   
   def initialize(filename)
     @filename = filename
   end
   
-  def [](ip, opts={})
-    if not ip =~ VALID_IP then
-      raise 'invalid ip' if opts[:raise]
-      return nil
-    end
+  def [](ip)
+    return nil unless ip =~ VALID_IP
+    parts = ip.split('.').map {|part| part.to_i}
     open(@filename) do |f|
       offset = 0
-      ip.split('.').each do |part|
-       start = offset + part.to_i * 4
-       f.seek start
-       value = f.read 4
+      parts.each do |part|
+       start = offset + part * 4
+       f.seek(start)
+       value = f.read(4)
        if value[0, 2] == "\xFF\xFF" then
-         if value[2, 2] == "\x00\x00" then
-           raise 'not found' if opts[:raise] 
-           return nil
-         end
+         return nil if value[2, 2] == "\x00\x00"
          return value[2, 2]
        end
        offset = value.unpack('N')[0]
@@ -33,9 +28,9 @@ class IP2CC
 end
 
 if __FILE__ == $0 then
-  db = IP2CC.new 'ip2cc.db'
+  db = Ip2cc.new 'ip2cc.db'
   if ARGV.length == 1 then
-    puts db[ARGV[0], :raise => true]
+    puts(db[ARGV[0]])
   else
     ARGV.each{ |ip| puts "#{ip} #{db[ip]}" }
   end
